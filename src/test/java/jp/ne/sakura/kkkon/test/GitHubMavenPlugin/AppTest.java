2a8e89f3a654b13575f16eea72f142756f6d492d
kkkon <kiyofumi.kondoh+github@gmail.com>
2013-12-09 07:07:27 +0900

    add app

---
+++ src/test/java/jp/ne/sakura/kkkon/test/GitHubMavenPlugin/AppTest.java
@@ -0,0 +1,97 @@
+/**
+ * The MIT License
+ * 
+ * Copyright (C) 2013 Kiyofumi Kondoh
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package jp.ne.sakura.kkkon.test.GitHubMavenPlugin;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kiyofumi Kondoh
+ */
+public class AppTest {
+    
+    public AppTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class App.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        App.main(args);
+    }
+
+    /**
+     * Test of main method, of class App.
+     */
+    @Test
+    public void testMainAndroidDryRun() {
+        System.out.println("mainAndroidDryRun");
+        String[] args = {
+            "-B"
+            , "--recursive"
+            , "target/test-classes/elf/android"
+            , "-o target/test-classes/elf-strip/android"
+            , "--dry-run"
+        };
+        App.main(args);
+    }
+    /**
+     * Test of main method, of class App.
+     */
+    @Test
+    public void testMainAndroid() {
+        System.out.println("mainAndroid");
+        String[] args = {
+            "-B"
+            , "--recursive"
+            , "target/test-classes/elf/android"
+            , "-o target/test-classes/elf-strip/android"
+        };
+        App.main(args);
+    }
+}
